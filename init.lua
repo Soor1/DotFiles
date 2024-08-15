70989722ae8f5d49bf63d9ebfe2cda4050a713e7
Soor Hansalia <128494314+Soor1@users.noreply.github.com>
2024-08-15 13:36:18 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,46 @@
+-- Set Leader Key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Tab Settings
+vim.opt.expandtab = true -- tab makes spaces instead of tabs
+vim.opt.softtabstop = 4 -- 4 spaces inserted instead of tab
+vim.opt.shiftwidth = 4 -- indenting is 4 spaces
+
+-- Set Line Number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Make nvim copy the same as OS copy
+vim.opt.clipboard = 'unnamedplus'
+
+-- Setting Delete Keymaps
+vim.api.nvim_set_keymap('n', 'dfw', 'ggdG', { noremap = true, silent = true }) -- Delete Whole File 
+vim.api.nvim_set_keymap('n', 'dw', 'daw', { noremap = true, silent = true }) -- Delete Word No Matter Curson Pos
+
+-- Setting Copy Key
+vim.api.nvim_set_keymap('n', 'cc', 'yy', { noremap = true, silent = true }) -- Copy Line
+vim.api.nvim_set_keymap('n', 'cap', 'yap', { noremap = true, silent = true }) -- Copy Around Paragraph
+vim.api.nvim_set_keymap('n', 'cfw', 'ggVGy', { noremap = true, silent = true }) -- Copy Whole File
+
+-- Yank Around Paragraph Highlight Function
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Install lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- Install lazy.nvim Packages
